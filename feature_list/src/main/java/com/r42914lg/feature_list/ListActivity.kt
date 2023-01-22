805f4d36a89bf055b6bb1ff202169a49ff2d9b44
lgdars22 <lgdars22@gmail.com>
2023-01-22 14:16:41 +0300

    initial

---
+++ feature_list/src/main/java/com/r42914lg/feature_list/ListActivity.kt
@@ -0,0 +1,11 @@
+package com.r42914lg.feature_list
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+    }
+}
